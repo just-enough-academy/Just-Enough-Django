30a38504ba42adedd98e108241759a73ae4000df
David Rogers <hello@al-the-x.me>
2016-05-23 14:10:51 -0700

    Add inital `Task` model and migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    """
+    Represents a textual reminder of what I need to do and whether I've
+    successfully completed it or not. Records the date and time of its
+    creation so it can be listed by oldest items first.
+
+    >>> Task.objects.count()
+    0
+    
+    >>> do_something = Task.objects.create(text='Do something!')
+    >>> do_something.complete
+    False
+    
+    >>> import datetime
+    >>> do_something.created_on is datetime.datetime.now()
+    True
+
+    >>> do_something.text
+    "Do something!"
+
+    >>> Task.objects.count()
+    1
+    
+    >>> Task.objects.first() == do_something
+    True
+
+    >>> Task.objects.count(completed=True)
+    0
+
+    >>> do_something.created = True
+    >>> do_something.save()
+    >>> len(Task.objects.filter(completed=True))
+    1
+    """
+
+    text = models.TextField()
+
+    complete = models.BooleanField(default=False, null=False)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+
