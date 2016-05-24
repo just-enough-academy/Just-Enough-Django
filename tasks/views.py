4b660216481a575a48f168df2b5a7fd9e9fe2ea0
David Rogers <hello@al-the-x.me>
2016-05-24 12:09:03 -0700

    Define Generic Views for Task: Browse, Edit, Add, Delete (no Read)

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.conf.urls import url, include
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Task
 
 def hello_world(request):
@@ -20,11 +20,32 @@ def task_detail(request, task_id):
         'task': Task.objects.get(pk=task_id) 
     })
 
+class TaskView(View):
+    success_url='..'
+    model=Task
+    fields=('text', 'complete')
+
+class TaskAddView(TaskView, CreateView):
+    pass
+
+class TaskUpdateView(TaskView, UpdateView):
+    pass
+    
+class TaskDetailView(TaskView, DetailView):
+    pass
+
+class TaskListView(TaskView, ListView):
+    pass
+    
+class TaskDeleteView(TaskView, DeleteView):
+    pass
+
 urlpatterns = [
-    url(r'^new', hello_world), # ADD(GET form)
-    url(r'^(?P<task_id>[0-9]+)', include([
-        url(r'^', task_detail), # READ, EDIT(POST form), DELETE?
-        url(r'^edit', hello_world) # EDIT(GET form)
+    url(r'^new', TaskAddView.as_view(), name='task_add'), # ADD(GET form)
+    url(r'^(?P<pk>[0-9]+)/', include([
+        url(r'^edit', TaskUpdateView.as_view(), name='task_edit'), # EDIT(GET form)
+        #url(r'^', TaskDetailView.as_view(), name='task_detail'), # READ, EDIT(POST form), DELETE?
+        url(r'^kill', TaskDeleteView.as_view(), name='task_delete'),
     ])),
-    url(r'^', ListView.as_view(model=Task), name='task_list'), # BROWSE, ADD(POST form)
+    url(r'^', TaskListView.as_view(), name='task_list'), # BROWSE, ADD(POST form)
 ]
