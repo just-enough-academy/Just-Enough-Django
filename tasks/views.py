12f680f873992192100ed5862f899b7ff5daf7ef
David Rogers <hello@al-the-x.me>
2016-05-24 09:14:16 -0700

    Add simple views for listing tasks and individiual task

--- tasks/views.py
+++ tasks/views.py
@@ -3,21 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 from django.conf.urls import url, include
 from django.http import HttpResponse
-from django.core.serializers import serialize
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Task
 
 def hello_world(request):
     return HttpResponse('Hello, World!')
 
-def list_tasks(request):
-    return HttpResponse(serialize('json', Task.objects.all()), 
-        content_type='application/json')
+def task_list(request):
+    return render(request, 'task_list.html', { 
+        'tasks': Task.objects.all() 
+    })
+
+def task_detail(request, task_id):
+    return render(request, 'task_detail.html', { 
+        'task': Task.objects.get(pk=task_id) 
+    })
 
 urlpatterns = [
-    url(r'^', list_tasks), # BROWSE, ADD(POST form)
     url(r'^new', hello_world), # ADD(GET form)
-    url(r'^(?P<id>[0-9]+)', include([
-        url(r'^', hello_world), # READ, EDIT(POST form), DELETE?
+    url(r'^(?P<task_id>[0-9]+)', include([
+        url(r'^', task_detail), # READ, EDIT(POST form), DELETE?
         url(r'^edit', hello_world) # EDIT(GET form)
-    ]))
+    ])),
+    url(r'^', ListView.as_view(model=Task), name='task_list'), # BROWSE, ADD(POST form)
 ]
